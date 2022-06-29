292c8e987951e60c07bf4766f1718651bc6aaf6c
asifmacmini <asif@thinkific.com>
2022-06-29 15:38:56 +0530

    Added navigation component.

--- app/src/main/java/com/kotproj/mvvmsearchpicfieasif/data/UnsplashPhoto.kt
+++ app/src/main/java/com/kotproj/mvvmsearchpicfieasif/data/UnsplashPhoto.kt
@@ -1,4 +1,4 @@
-package com.kotproj.mvvmsearchpicfieasif
+package com.kotproj.mvvmsearchpicfieasif.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
---
+++ app/src/main/java/com/kotproj/mvvmsearchpicfieasif/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.kotproj.mvvmsearchpicfieasif.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.kotproj.mvvmsearchpicfieasif.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
